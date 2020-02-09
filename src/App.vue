7f9f40502a6870232ae53b5f35f7d91d49b955de
Jess Archer <jess@jessarcher.com>
2020-02-09 22:38:05 +1000

    Add icon examples to the home page

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,17 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="p-10 text-xl">
+    <BaseIcon name="home" class="text-blue-500" /> Home
+    <BaseIcon name="user" class="text-blue-500" /> Profile
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BaseIcon from './components/BaseIcon.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BaseIcon
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
