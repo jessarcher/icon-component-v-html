c9e595e861c8c017c92c909ac8e938438b6f23e0
Jess Archer <jess@jessarcher.com>
2020-02-09 22:37:53 +1000

    Add BaseIcon component with home and user icons

---
+++ src/components/BaseIcon.vue
@@ -0,0 +1,18 @@
+<template>
+  <div
+    v-html="require(`../assets/icons/icon-${name}.svg`)"
+    class="inline-block fill-current"
+    style="height: 1em; width: 1em; vertical-align: -0.125em;"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+    }
+  }
+}
+</script>
